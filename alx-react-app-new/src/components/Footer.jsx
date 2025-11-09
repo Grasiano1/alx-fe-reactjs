0bc90245d017f768b632d06cb9581a218cb6289c
Grasiano Dery <dewgrass77@gmail.com>
2025-11-09 23:30:52 +0000

    build a counter

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer style={{color: 'black', fontSize: '24px'}}>
+            <p>&copy; 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+        <main style={{border: '1px solid gray', fontSize: '20px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
